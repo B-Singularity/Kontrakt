f970f81c0bf870c4f7282efd2f0d4dfbada1b721
B-singularity <skil1489@naver.com>
2025-10-09 16:28:41 +0900

    feat(discovery): add aggregate root

--- kontrakt-core/src/main/kotlin/discovery/domain/aggregate/TestSpecification.kt
+++ kontrakt-core/src/main/kotlin/discovery/domain/aggregate/TestSpecification.kt
@@ -1,4 +1,18 @@
 package discovery.domain.aggregate
 
-class TestSpecification {
+import discovery.domain.vo.DependencyMetadata
+import discovery.domain.vo.DiscoveredTestTarget
+
+data class TestSpecification private constructor(
+    val target: DiscoveredTestTarget,
+    val requiredDependencies: List<DependencyMetadata>
+) {
+    companion object {
+        fun create(
+            target: DiscoveredTestTarget,
+            requiredDependencies: List<DependencyMetadata>
+        ): Result<TestSpecification> {
+            return Result.success(TestSpecification(target, requiredDependencies))
+        }
+    }
 }
--- kontrakt-core/src/main/kotlin/discovery/domain/vo/DependencyMetadata.kt
+++ kontrakt-core/src/main/kotlin/discovery/domain/vo/DependencyMetadata.kt
@@ -2,7 +2,19 @@ package discovery.domain.vo
 
 import kotlin.reflect.KClass
 
-data class DependencyMetadata(
+data class DependencyMetadata private constructor(
     val name: String,
     val type: KClass<*>
-)
+) {
+    companion object {
+        fun create(
+            name: String,
+            type: KClass<*>
+        ): Result<DependencyMetadata> {
+            if (name.isBlank()) {
+                return Result.failure(IllegalArgumentException("Name cannot be blank"))
+            }
+            return Result.success(DependencyMetadata(name, type))
+        }
+    }
+}
