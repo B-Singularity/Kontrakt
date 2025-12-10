d1307554ec2f1b5f23835db5fc4aa2fa1891aacd
B-singularity <skil1489@naver.com>
2025-12-11 01:01:53 +0900

    feat: TestDiscovererTest impl

---
+++ kontrakt-core/src/test/kotlin/discovery/domain/service/TestDiscovererImplTest.kt
@@ -0,0 +1,21 @@
+package discovery.domain.service
+
+import discovery.api.TestDiscoverer
+import discovery.api.TestDiscovererTest
+import discovery.fakes.FakeClasspathScanner
+import kotlin.reflect.KClass
+
+class TestDiscovererImplTest : TestDiscovererTest() {
+
+    private val fakeScanner = FakeClasspathScanner()
+
+    override val discoverer: TestDiscoverer = TestDiscovererImpl(fakeScanner)
+
+    override fun setupScanResult(
+        interfaces: List<KClass<*>>,
+        classes: List<KClass<*>>,
+        implementations: Map<KClass<*>, List<KClass<*>>>
+    ) {
+        fakeScanner.setup(interfaces, classes, implementations)
+    }
+}
---
+++ kontrakt-core/src/test/kotlin/discovery/fakes/FakeClasspathScanner.kt
@@ -0,0 +1,43 @@
+package discovery.fakes
+
+import discovery.domain.vo.ScanScope
+import discovery.spi.ClasspathScanner
+import kotlin.reflect.KClass
+
+class FakeClasspathScanner : ClasspathScanner {
+
+    private var interfaces: List<KClass<*>> = emptyList()
+    private var classes: List<KClass<*>> = emptyList()
+    private var implementations: Map<KClass<*>, List<KClass<*>>> = emptyMap()
+
+    fun setup(
+        interfaces: List<KClass<*>> = emptyList(),
+        classes: List<KClass<*>> = emptyList(),
+        implementations: Map<KClass<*>, List<KClass<*>>> = emptyMap()
+    ) {
+        this.interfaces = interfaces
+        this.classes = classes
+        this.implementations = implementations
+    }
+
+    override suspend fun findAnnotatedInterfaces(
+        scope: ScanScope,
+        annotation: KClass<out Annotation>
+    ): List<KClass<*>> {
+        return interfaces
+    }
+
+    override suspend fun findAnnotatedClasses(
+        scope: ScanScope,
+        annotation: KClass<out Annotation>
+    ): List<KClass<*>> {
+        return classes
+    }
+
+    override suspend fun findAllImplementations(
+        scope: ScanScope,
+        targetInterface: KClass<*>
+    ): List<KClass<*>> {
+        return implementations[targetInterface] ?: emptyList()
+    }
+}
